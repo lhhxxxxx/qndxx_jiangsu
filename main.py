e040ff9e6bb8f514dd725476a1def766a3123aec
lhhxxxxx <91231470+lhhxxxxx@users.noreply.github.com>
2023-03-09 15:37:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from Qndxx import Qndxx
 def schedule_start():
     laravel_session = ""
     qndxx = Qndxx(laravel_session)
-    qndxx.login()
-    qndxx.confirm()
+    qndxx.start()
 
 
 def func():
     logger.info("schedule start")
     schedule_start()
-    timer = threading.Timer(86400,func)
+    timer = threading.Timer(86400, func)
     timer.start()
 
-timer = threading.Timer(0,func)
+
+timer = threading.Timer(0, func)
 timer.start()
